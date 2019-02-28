980d2e8b5ea9936ad6d18bedd41b2fee48ace2c1
vincentcote87 <vincentcote87@gmail.com>
2019-02-27 22:41:48 -0700

    Added comments and revert back to original family data

--- family.pl
+++ family.pl
@@ -1,114 +1,126 @@
-male(vincent).
-male(tighe).
-male(brodie).
-male(reg).
-male(dany).
-male(rodolph).
-male(jeff).
-male(michel).
-male(gerald).
-male(mario).
-male(julien).
+male(bill).
+male(john).
+male(frank).
+male(joe).
+male(larrie).
+male(philip).
+male(kevin).
+male(chris).
+male(danny).
 
-female(krista).
-female(hailie).
-female(sus).
-female(celine).
-female(valerie).
-female(gyslene).
-female(monique).
-female(dianne).
+female(anne).
+female(dorothy).
+female(sharon).
+female(rochelle).
+female(danelle).
+female(marg).
+female(connie).
+female(corrie).
 
-parent(vincent,brodie).
-parent(vincent,tighe).
-parent(vincent,hailie).
-parent(krista,brodie).
-parent(krista,tighe).
-parent(krista,hailie).
-parent(reg,krista).
-parent(sus,krista).
-parent(dany,vincent).
-parent(dany,valerie).
-parent(dany,jeff).
-parent(celine,vincent).
-parent(celine,valerie).
-parent(celine,jeff).
-parent(rodolph,celine).
-parent(gyslene,celine).
-parent(rodolph,mario).
-parent(gyslene,mario).
-parent(gerald,michel).
-parent(monique,michel).
-parent(mario,julien).
-parent(dianne,julien).
+parent(bill,john).
+parent(bill,frank).
+parent(bill,joe).
+parent(anne,john).
+parent(anne,frank).
+parent(anne,joe).
+parent(john,larrie).
+parent(john,sharon).
+parent(john,philip).
+parent(john,kevin).
+parent(dorothy,larrie).
+parent(dorothy,sharon).
+parent(dorothy,philip).
+parent(dorothy,kevin).
+parent(larrie,connie).
+parent(larrie,corrie).
+parent(larrie,chris).
+parent(marg,connie).
+parent(marg,corrie).
+parent(marg,chris).
+parent(danny,rochelle).
+parent(danny,danelle).
+parent(sharon,rochelle).
+parent(sharon,danelle).
 
-
+% X is a male parent of Y
 father(X,Y) :-
   male(X),
   parent(X,Y).
 
+% X is a female parent of Y
 mother(X,Y) :-
   female(X),
   parent(X,Y).
 
+% X is a male child of Y
 son(X,Y) :-
   male(X),
   parent(Y,X).
 
+% X is a female child of Y
 daughter(X,Y) :-
   female(X),
   parent(Y,X).
 
-% goes through twice since son returns two parents.
+% X is the son of the same mother or father of Y
 brother(X,Y) :-
-%  \+ (X = Y),
   son(X,Z),
-  parent(Z,Y).
+  parent(Z,Y),
+  \+ (X = Y).
 
-% goes through twice since daughter returns two parents.
+% X is the daughter of the same mother or father of Y
 sister(X,Y) :-
-%  \+ (X = Y),
   daughter(X,Z),
-  parent(Z,Y).
+  parent(Z,Y),
+  \+ (X = Y).
 
+% X is the father of Y’s father or mother
 grandfather(X,Y) :-
   male(X),
   parent(X,Z),
   parent(Z,Y).
 
+% X is the mother of Y’s father or mother
 grandmother(X,Y) :-
   female(X),
   parent(X,Z),
   parent(Z,Y).
 
+% X is the son of Y’s son or daughter
 grandson(X,Y) :-
   male(X),
   (grandfather(Y,X);
    grandmother(Y,X)).
 
+% X is the daughter of Y’s son or daughter
 granddaughter(X,Y) :-
   female(X),
   (grandfather(Y,X);
    grandmother(Y,X)).
 
+% X is the brother of Y’s father or mother
 uncle(X,Y) :-
   parent(Z,Y),
   brother(X,Z).
 
+% X is the sister of Y’s father or mother
 aunt(X,Y) :-
   parent(Z,Y),
   sister(X,Z).
 
+% X is the son of Y’s brother or sister
 nephew(X,Y) :-
   male(X),
   (uncle(Y,X);
    aunt(Y,X)).
 
+% X is the daughter of Y’s brother or sister
 niece(X,Y) :-
   female(X),
   (uncle(Y,X);
    aunt(Y,X)).
 
+ % X is the son or daughter of Y’s uncle or aunt
 cousin(X,Y) :-
   (son(X,Z);
    daughter(X,Z)),
@@ -116,6 +128,7 @@ cousin(X,Y) :-
    aunt(Z,Y)),
    \+ (X = Y).
 
+% Y descends from X
 % base case of recursion
 ancestor(X,Y) :-
   parent(X,Y).
@@ -125,5 +138,6 @@ ancestor(X,Y) :-
   parent(X,Z),
   ancestor(Z,Y).
 
+% X descends from Y
 descendant(X,Y) :-
   ancestor(Y,X).
